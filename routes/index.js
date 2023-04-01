abe5686f9481e59bdbf736aff62b01f010a04bab
malgumi <malgumizzang@naver.com>
2023-04-01 23:04:27 +0900

    git 충돌 해결

--- routes/index.js
+++ routes/index.js
@@ -7,12 +7,10 @@ const user = require('./user/index');
 router.use('/post', post);
 router.use('/user', user);
 
-<<<<<<< HEAD
+
 module.exports = router;
 //test용 주석
-=======
+
 
 module.exports = router;
 
-//테스트용 주석
->>>>>>> 158bd02170cd14a2b769cb59e0513068bb9e51e3
