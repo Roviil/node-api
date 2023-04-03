f5dcb2c194e440773c297eef21bfe0410f199975
malgumi <malgumizzang@naver.com>
2023-04-04 02:17:20 +0900

    댓글 권한 거르기

--- routes/post/post.controller.js
+++ routes/post/post.controller.js
@@ -203,14 +203,15 @@ exports.deleteComment = (req, res) => {
     const token = req.decoded; // 헤더에서 토큰 추출
 
     try {
-      const student_id = token.student_id; // 사용자 ID 추출
+      const student_id = token.student_id; // 삭제버튼 누른 사용자 ID 추출
+      const select_student_id = "SELECT student_id FROM comment WHERE comment_id = ?"; //댓글 사용자 ID 추출
       const sql = "UPDATE comment SET available=0 WHERE comment_id=? AND student_id=?"; // SQL 쿼리
       const values = [comment_id, student_id]; // SQL 쿼리 값
       db.query(sql, values, (error, results) => {
         if (error) {
           console.error("댓글 삭제 실패: ", error);
           res.status(500).json({ message: "서버 내부 오류" });
-        } else if (!error){
+        } else if (select_student_id == student_id){
           console.log("댓글 삭제 성공!");
           res.status(200).json({ message: "댓글이 삭제되었습니다." });
         }
