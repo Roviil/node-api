9b216cd35d9c2274228fcd3d5b24a7bbeb41bd69
Roviil <qowjdgns0106@naver.com>
2023-04-04 00:18:04 +0900

    컨트롤러 오류 수정

--- routes/post/post.controller.js
+++ routes/post/post.controller.js
@@ -116,8 +116,7 @@ exports.commentget = (req, res) => {
       console.log(error);
       res.status(500).send('Internal Server Error');
     } else {
-      results.length
-      res.status(200).json(results, results.length);
+      res.status(200).json(results);
     }
   });
 }
