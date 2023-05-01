aa305b897ecf66c072d248e01c4e8b5c6146790c
malgumi <malgumizzang@naver.com>
2023-05-01 22:10:47 +0900

    수정 기능 구현

--- routes/post/post.controller.js
+++ routes/post/post.controller.js
@@ -253,6 +253,48 @@ exports.deleteComment = (req, res) => {
   });
 };
 
+exports.updateComment = (req, res) => {
+  verifyToken(req, res, () => {
+    const comment_id = req.params.comment_id;
+    const comment_content = req.body.comment_content;
+    const token = req.decoded; // 헤더에서 토큰 추출
+
+    try {
+      const student_id = token.student_id; // 삭제버튼 누른 사용자 ID 추출
+      const selectsql = "SELECT student_id FROM comment WHERE comment_id = ?"; //댓글 사용자 ID 추출
+      const sql = "UPDATE comment SET comment_content=? WHERE comment_id=? AND student_id=?"; // SQL 쿼리
+      const values = [comment_content, comment_id, student_id]; // SQL 쿼리 값
+      // 댓글 내용이 비어있는 경우
+      if (comment_content == null || comment_content.isEmpty) {
+        console.error("내용이 비어있습니다.");
+        res.status(500).json({ message: "내용을 입력해주세요." });
+        return;
+      }
+      db.query(selectsql, comment_id, (error, select_student_id) => {
+        if (error) {
+          console.error("뭔가 오류임.", error);
+          res.status(500).json({ message: "서버 내부 오류" });
+        }
+        db.query(sql, values, (error, results) => {
+          if (error) {
+            console.error("댓글 삭제 실패: ", error);
+            res.status(500).json({ message: "서버 내부 오류" });
+          } else if (select_student_id[0].student_id == student_id){
+            console.log("댓글 삭제 성공!");
+            res.status(200).json({ message: "댓글이 삭제되었습니다." });
+          }
+          else{
+            console.log("댓글 삭제 권한 없음");
+            res.status(300).json({ message: "권한이 없습니다." });
+          }
+        });
+      });
+    } catch (err) {
+      console.error("토큰 검증 실패: ", err);
+      res.status(401).json({ message: "토큰이 유효하지 않습니다." });
+    }
+  });
+};
 
 exports.updatePost = (req, res) => {
   verifyToken(req, res, () => {
