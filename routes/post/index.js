9c70ff52cf13749fc196e65a2c36f7107b6590a1
Roviil <qowjdgns0106@naver.com>
2023-05-15 23:28:38 +0900

    test

--- routes/post/index.js
+++ routes/post/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const controller = require('./post.controller');
 
-/* GET users listing. */
+/* GET users listing. * test/
 
 /**
  * @swagger
