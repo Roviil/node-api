3e24239e9e9f2ee0e2424bf4b7f771f089c09115
배정훈 <qowjdgns0106@naver.com>
2024-04-08 15:05:04 +0900

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const cors = require("cors");
 
 var app = express();
 
-// view engine setup
+// view engine setup test
 app.use(cors());
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
